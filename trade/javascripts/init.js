6edad0b66a2a2eda1bce600086ef69b496c711f0
Winton Welsh <win@sumapp.com>
2008-07-26 21:56:50 -0500

    No longer have to use a delay on trade init js, child widgets now order properly

--- trade/javascripts/init.js
+++ trade/javascripts/init.js
@@ -1,18 +1,16 @@
-(function() {
-  [ 'trade_table_mine', 'trade_table_theirs' ].each(function(item) {
-    Global[item].addEvents({
-      newComplete: function(el, js) {
-        Global.dialog.render({ element: el });
-        eval(js);
-      },
-      editComplete: function(el, js) {
-        Global.dialog.render({ element: el });
-        eval(js);
-      },
-      deleteComplete: function(json) {},
-      reloaded: function() {
-        Global.<%= id %> = new Trade($merge(<%= options.to_json %>, { table_id: item }));
-      }
-    });
+[ 'trade_table_mine', 'trade_table_theirs' ].each(function(item) {
+  Global[item].addEvents({
+    newComplete: function(el, js) {
+      Global.dialog.render({ element: el });
+      eval(js);
+    },
+    editComplete: function(el, js) {
+      Global.dialog.render({ element: el });
+      eval(js);
+    },
+    deleteComplete: function(json) {},
+    reloaded: function() {
+      Global.<%= id %> = new Trade($merge(<%= options.to_json %>, { table_id: item }));
+    }
   });
-}).delay(100);
+});
