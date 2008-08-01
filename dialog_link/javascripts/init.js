3e636addf21ce4fd781aed047f7a1896e609be7c
Winton Welsh <win@sumapp.com>
2008-08-01 13:41:51 -0500

    Adding dialog_id to dialog_link options

--- dialog_link/javascripts/init.js
+++ dialog_link/javascripts/init.js
@@ -2,13 +2,13 @@
 <% id       = template.respond_to?(:keys) ? template.keys.first : template -%>
 <% template = template.respond_to?(:keys) ? template[id]        : template -%>
 $('<%= id %>').addEvent('click', function(e) {
-  Global.dialog.render({ element: $('template_<%= template %>').render(<%= options.to_json %>) });
+  Global['<%= dialog_id %>'].render({ element: $('template_<%= template %>').render(<%= options.to_json %>) });
   return false;
 });
 <% end -%>
 <% urls.each do |id, url| -%>
 $('<%= id %>').addEvent('click', function(e) {
-  Global.dialog.render({
+  Global['<%= dialog_id %>'].render({
     url: <%= url ? url.inspect : 'null' %> || this.get('href'),
     data: {
       authenticity_token: Global.authenticity_token,
--- dialog_link/options.rb
+++ dialog_link/options.rb
@@ -1,4 +1,5 @@
 {
+  :dialog_id => 'dialog',
  #:templates => [ :id_and_template, { :dom_id_here => 'template' } ],
   :templates => [],
  #:urls      => { :dom_id_here => '/some_resource' }
