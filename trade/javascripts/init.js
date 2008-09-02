1da37e6191a6c5c9c289e2a2abcd1cebd732d7b8
Winton Welsh <win@sumapp.com>
2008-09-02 14:43:17 -0500

    Copying local commits

--- trade/javascripts/init.js
+++ trade/javascripts/init.js
@@ -1,16 +1,62 @@
-[ 'trade_table_mine', 'trade_table_theirs' ].each(function(item) {
-  Global[item].addEvents({
-    newComplete: function(el, js) {
-      Global.dialog.render({ element: el });
-      eval(js);
-    },
-    editComplete: function(el, js) {
-      Global.dialog.render({ element: el });
-      eval(js);
-    },
-    deleteComplete: function(json) {},
-    reloaded: function() {
-      Global.<%= id %> = new Trade($merge(<%= options.to_json %>, { table_id: item }));
-    }
+if (!$('<%= id %>').hasClass('accepted')) {
+  [ 'trade_table_mine', 'trade_table_theirs' ].each(function(item) {
+    Global[item].addEvents({
+      newComplete: function(el, js) {
+        Global.dialog.render({ element: el });
+        eval(js);
+      },
+      editComplete: function(el, js) {
+        Global.dialog.render({ element: el });
+        eval(js);
+      },
+      deleteComplete: function(json) {},
+      reloaded: function() {
+        Global[item + '_<%= id %>'] = new Trade($merge(<%= options.to_json %>, { table_id: item }));
+      }
+    });
   });
-});
+  $('<%= id %>').getElement('.send').addEvent('click', function() {
+    Global.indicator.show();
+    new Request.HTML({
+      url: '/trades',
+      method: 'post',
+      data: {
+        'trade[trade_to_id]': this.id,
+        'trade[from_ids]': Global.trade_table_mine_trade_builder.getDraggedIDs(),
+        'trade[to_ids]': Global.trade_table_theirs_trade_builder.getDraggedIDs(),
+        implementation: 'eborhood/widgets/form/dialog/trade',
+        authenticity_token: Global.authenticity_token
+      },
+      evalScripts: false,
+      onSuccess: function(tree, elements, html, js) {
+        Global.dialog.render({ element: elements[0] });
+        eval(js);
+        Global.indicator.hide();
+      }
+    }).send();
+  });
+  var accept = $('<%= id %>').getElement('.accept');
+  if (accept)
+    accept.addEvent('click', function() {
+      Global.indicator.show();
+      new Request.HTML({
+        url: '/trades/' + this.id,
+        method: 'post',
+        data: {
+          '_method': 'put',
+          'trade[accepted]': true,
+          authenticity_token: Global.authenticity_token
+        },
+        evalScripts: false,
+        onSuccess: function(tree, elements, html, js) {
+          var table = $('trades_table');
+          var next  = table.getNext();
+          if (next) next.destroy();
+          table.hide();
+          elements[0].inject(table, 'after');
+          eval(js);
+          Global.indicator.hide();
+        }
+      }).send();
+    });
+}
--- trade/javascripts/trade.js
+++ trade/javascripts/trade.js
@@ -4,7 +4,35 @@ var Trade = new Class({
     var container = $(options.table_id);
     var draggables = container.getElements('.rows_cell');
     var droppables = $$(options.table_id.contains('mine') ? '.mine_target' : '.theirs_target');
-    var total = container.getElement('.total');
+    var total;
+    
+    var refreshTotal = function(droppable, price) {
+      total = droppable.getElement('.total');
+      if (!total) {
+        total = new Element('div', {
+          'class': 'total',
+          html: '= $' + price
+        });
+        total.inject(droppables[0], 'top');
+      } else {
+        price += total.textContent.replace(/\D/g, '').toFloat();
+        total.innerHTML = '= $' + price;
+      }
+    };
+    
+    var attachMove = function(div, price) {
+      new Drag.Move(div, {
+        onDrop: function(element) {
+          price -= element.get('price');
+          total.innerHTML = '= $' + price;
+          if (price == 0) {
+            total.destroy();
+            total = null;
+          }
+          element.destroy();
+        }
+      });
+    };
     
     draggables.each(function(item) {
       new Drag.Move(item, {
@@ -26,32 +54,13 @@ var Trade = new Class({
             });
             droppable.setStyle('background', '#F0EFEC');
             if ($(div.id)) return;
-            if (!total) {
-              total = new Element('div', {
-                'class': 'total',
-                html: '= $' + price
-              });
-              total.inject(droppables[0], 'top');
-            } else {
-              price += total.textContent.replace(/\D/g, '').toFloat();
-              total.innerHTML = '= $' + price;
-            }
+            refreshTotal(droppable, price);
             if (img) {
               new Element('br').inject(div, 'top');
               img.clone().inject(div, 'top');
             }
             div.inject(droppables[0], 'bottom');
-            new Drag.Move(div, {
-              onDrop: function(element) {
-                price -= element.get('price');
-                total.innerHTML = '= $' + price;
-                if (price == 0) {
-                  total.destroy();
-                  total = null;
-                }
-                element.destroy();
-              }
-            });
+            attachMove(div, price);
           }
         },
         onEnter: function(element, droppable) {
@@ -62,6 +71,17 @@ var Trade = new Class({
           element.setStyles({ top:0, left:0 });
         }
       });
+    });
+    
+    this.getDraggedIDs = function() {
+      return droppables[0].getChildren().map(function(div) {
+        return div.id.replace(/\D/g, '');
+      }).clean().join(",");
+    };
+    
+    droppables[0].getElements('.item').each(function(item) {
+      refreshTotal(droppables[0], item.get('price').toFloat());
+      attachMove(item, item.get('price'));
     });
   }
 });
